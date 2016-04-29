2af5b5a2c80bc7958ac04d11387e7de218d26958
Tom Walsh <fathomswalsh@verizon.net>
2016-04-29 15:59:48 -0400

    Added a few more tweaks to warning messages

--- js/diveCalculator.js
+++ js/diveCalculator.js
@@ -1,22 +1,20 @@
 $(document).on("pageinit", "#main", function(event) {
-	$("span#BM_value").text(70);	     					//On document ready default is Imperial units
-	$("span#MOD_value").text(187);	     					//This sets default values of output in the four tabs 
+	$("span#BM_value").text(70);	     							//On document ready default is Imperial units
+	$("span#MOD_value").text(187);	     							//These sets default values of output in the four tabs 
 	$("span#EAD_value").text(33);	
 	$("span#ppO2_value").text(0.42);
-	ppO2Max = 1.4;											//and sets default value of global variable ppO2Max
-	DepthConst = 33;										//and set default value of DepthConst (Depth in feet or Meters = 1 atmosphere)
-	$("#EADmsg").hide();
-	$("#PPO2msg").hide();
+	ppO2Max = 1.4;													//and sets default value of global variable ppO2Max
+	DepthConst = 33;												//and sets default value of DepthConst (Depth in feet or Meters = 1 atmosphere)
 	});
 
-$(document).on('change', '#flip-1', function() {			//Monitor changes to maximum partial pressure of oxygen
-	ppO2Max = $("#flip-1").val();							//and recalculate Best Mix and Maximum Operating Depth
+$(document).on('change', '#flip-1', function() {					//Monitor changes to maximum partial pressure of oxygen
+	ppO2Max = $("#flip-1").val();									//and recalculate Best Mix and Maximum Operating Depth
 	calcBM();								
 	calcMOD();	
 	});	
 
-$(document).on('change', '#flip-2', function() {			//Monitor selection of Imperial or Metric units
-	var selection = $("#flip-2").val();						//and call appropriate function to handle the display
+$(document).on('change', '#flip-2', function() {					//Monitor selection of Imperial or Metric units
+	var selection = $("#flip-2").val();								//and call appropriate function to handle the display
 	$("#units").html(selection);		
 	if (selection == "Metric")  {
 		chgToMetric();}		
@@ -24,17 +22,17 @@ $(document).on('change', '#flip-2', function() {			//Monitor selection of Imperi
 		chgToImperial();}	
 	});	
 
-function calcBM() {
+function calcBM() {													//Calculates Best Mix for a dive to a given depth
 	var BMdepth = $("#BMdepth_slider").val();
 	BM = Math.round(100 * ((ppO2Max)/((BMdepth/DepthConst) + 1)));	
 	$('#BM_value').html(BM);}
 
-function calcMOD() {
+function calcMOD() {												//Calculates Maximum Operating Depth of an EANx blend
 	var MODfO2 = $("#MODpercentO2slider").val()/100;	
 	MOD = Math.round(DepthConst * ((ppO2Max/MODfO2) - 1));	
 	$("#MOD_value").html(MOD);}
 		
-function calcEAD() {
+function calcEAD() {												//Calculates the Equivalent Air Depth for an EANx blend at a selected depth
 	var EADfO2 = ($("#EADpercentO2slider").val())/100;
 	var EADdepth = $("#EADdepth_slider").val();
 	var fN2 = (1 - EADfO2);	
@@ -42,54 +40,46 @@ function calcEAD() {
 	EAD = Math.round((((fN2) * (new_depth))/.79) - DepthConst);
 	$('#EAD_value').html(EAD);
 	var ppO2 = (EADfO2 * ((EADdepth/DepthConst) + 1)).toFixed(2);
-	EADwarning(ppO2);}	
+	PPO2warning(ppO2);}												//Warns user if ppO2 values exceed safe limits
 
-function calcPPO2() {
+function calcPPO2() {												//Calculates the Partial Pressure of Oxygen for a given EANx blend and a selected depth
 	ppO2fO2 = ($("#ppO2percentO2slider").val())/100;
 	ppO2depth = $("#ppO2depth_slider").val();		
 	ppO2 = (ppO2fO2 * ((ppO2depth/DepthConst) + 1)).toFixed(2);
 	$("#ppO2_value").html(ppO2);
-	PPO2warning(ppO2);}	  	
+	PPO2warning(ppO2);}												//Warns user if ppO2 values exceed safe limits 	
 		
-function EADwarning(ppO2) {
+function PPO2warning(ppO2) {										//Tests for ppO2 values and issues the warnings
+	if (ppO2 <= 1.4) {
+	$(".msg").css("display","none");}	
 	if (ppO2 > 1.4 && ppO2 <= 1.6) {	
-	warning1()}
+	$(".msg").css("display","block");	
+	$(".msg").css("color","green");
+	$(".msg").html("Caution! Do not exceed 1.4 (ATA)/Bar during<br>the working part of the dive.<br>Use for decompression only.");}
 	if (ppO2 >= 1.6) {
-	warning2()}
-}		
+	$(".msg").css("display","block");	
+	$(".msg").css("color","red");
+	$(".msg").html("Danger! Partial pressures of oxygen<br>greater than 1.6 ATA/Bar<br>pose serious risk of siezure.");}}		
 
-function warning1() {
-$("#EADmsg").css("display","block");	
-$("#EADmsg").css("color","green");
-$("#EADmsg").html("Caution! Do not exceed 1.4 (ATA)/Bar during<br>the working part of the dive.<br>Use for decompression only.");}	
-
-function warning2() {
-$("#EADmsg").css("display","block");	
-$("#EADmsg").css("color","red");
-$("#EADmsg").html("Danger! Partial pressures of oxygen<br>greater than 1.6 ATA/Bar<br>pose serious risk of siezure.");}
-
-
-
-	
 function chgToMetric() {									
-	DepthConst = 10;										//set DepthConst to Metric value
-	$(".sliderLabel").text("Depth in Meters");				//change labels to Metric
+	DepthConst = 10;												//Set DepthConst to Metric value
+	$(".sliderLabel").text("Depth in Meters");						//Change labels to Metric
 	$(".pressureUnits").text("Bar");
 	$(".depthUnits").text("Meters");
 	$(".sliderValue").prop({min: 10, max: 45}).slider("refresh");	
-	//calcBM();												//Recalculate the four functions							
+	//calcBM();														//Recalculate the four functions							
 	//calcMOD();
 	//calcEAD();
 	//calcPPO2();
 	}	
 
 function chgToImperial() {									
-	DepthConst = 33;										//set DepthConst to Imperial value
-	$(".sliderLabel").text("Depth in Feet");				//change labels to Imperial
+	DepthConst = 33;												//set DepthConst to Imperial value
+	$(".sliderLabel").text("Depth in Feet");						//change labels to Imperial
 	$(".pressureUnits").text("Atmospheres Absolute (ATA)");
 	$(".depthUnits").text("Feet");
 	$(".sliderValue").prop({min: 33, max: 148}).slider("refresh");	
-	//calcBM();												//Recalculate the four functions								
+	//calcBM();														//Recalculate the four functions								
 	//calcMOD();
 	//calcEAD();
 	//calcPPO2();	
